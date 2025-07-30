da6f44448221ea3c07c7db06c9ad1bc413efd774
Shree Mengshetti <shreemengshetti12@gmail.com>
2025-07-30 12:58:05 +0530

    memory saved

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,20 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage,systemMessage,AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model =ChatOpenAI()
 
-while true:
+chatHistory =[
+    systemMessage(content="You are a helpful assistant."),
+]
+
+while True:
     user_input =input('You: ')
+    chatHistory.append(HumanMessage(content=user_input))
     if user_input.lower =='exit':
         break
     result=model.invoke(user_input)
+    chatHistory.append(AIMessage(content=result))
     print(f'Bot: {result}')
